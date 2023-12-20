63e24b42697f774b9f02e0c689786d696dbaf92a
CloudPlayer <2909078582@qq.com>
2023-12-20 20:46:56 +0800

    删除了一些无关文件

--- mybatis-flex-kotlin-codegen/src/test/kotlin/SuspendBaseMapperImpl.kt
+++
@@ -1,9 +0,0 @@
-import com.mybatisflex.core.BaseMapper
-import com.mybatisflex.kotlin.extensions.db.mapper
-
-@JvmInline
-value class SuspendBaseMapperImpl<T>(private val baseMapper: BaseMapper<T> = mapper()) : BaseMapper<T> by baseMapper, Thread.UncaughtExceptionHandler {
-    override fun uncaughtException(t: Thread, e: Throwable) {
-        println("The exception is $e, happened in $t")
-    }
-}
