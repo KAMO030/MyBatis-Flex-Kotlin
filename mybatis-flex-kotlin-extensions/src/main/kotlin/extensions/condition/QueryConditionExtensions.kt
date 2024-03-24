912a4312917c1fde9e20f811e4e9137b3fdc3027
kamosama <837080904@qq.com>
2024-03-24 11:51:18 +0800

    refactor: 移除空条件单例，改为方法获取

--- mybatis-flex-kotlin-extensions/src/main/kotlin/extensions/condition/QueryConditionExtensions.kt
+++ mybatis-flex-kotlin-extensions/src/main/kotlin/extensions/condition/QueryConditionExtensions.kt
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+@file:Suppress("unused")
 package com.mybatisflex.kotlin.extensions.condition
 
 import com.mybatisflex.core.query.QueryCondition
