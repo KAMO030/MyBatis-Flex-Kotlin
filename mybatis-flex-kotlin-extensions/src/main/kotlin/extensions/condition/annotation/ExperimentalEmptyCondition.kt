5e4539da55d0d7d3ed9f5f4c401fd33b86f49457
kamosama <837080904@qq.com>
2024-04-10 16:09:07 +0800

    build extensions v1.0.8

--- mybatis-flex-kotlin-extensions/src/main/kotlin/extensions/condition/annotation/ExperimentalEmptyCondition.kt
+++
@@ -1,5 +0,0 @@
-package com.mybatisflex.kotlin.extensions.condition.annotation
-
-@RequiresOptIn("EmptyCondition 单例仍在测试，请避免将其使用至生产项目中。" +
-        "如若遇到问题，请改用 QueryCondition.createEmpty() 方法。")
-annotation class ExperimentalEmptyCondition
