b5576e3f1329f276217264b84213a409ec365ad1
CloudPlayer <2909078582@qq.com>
2024-06-11 00:03:34 +0800

    更新 Kotlin poet 版本至 1.17.0 以图更好的性能

--- mybatis-flex-kotlin-codegen/build.gradle.kts
+++ mybatis-flex-kotlin-codegen/build.gradle.kts
@@ -3,7 +3,9 @@ version = libs.versions.mybatisflex.kotlin.codegen.get()
 dependencies {
     implementation(libs.mybatisflex.codegen)
     implementation(libs.mybatisflex.core)
-    implementation(libs.kotlinpoet)
+    // https://mvnrepository.com/artifact/com.squareup/kotlinpoet-jvm
+    implementation("com.squareup:kotlinpoet:1.17.0")
+
     testImplementation(libs.mysql)
 
     testImplementation(project(":mybatis-flex-kotlin-extensions"))
