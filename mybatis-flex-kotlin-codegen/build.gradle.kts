0c0216010a77c683ea39c2fd7e347238966ac507
kamosama <837080904@qq.com>
2024-06-13 16:15:30 +0800

    update(codegen):将核心库依赖设置为compileOnly

--- mybatis-flex-kotlin-codegen/build.gradle.kts
+++ mybatis-flex-kotlin-codegen/build.gradle.kts
@@ -2,7 +2,7 @@ version = libs.versions.mybatisflex.kotlin.codegen.get()
 
 dependencies {
     implementation(libs.mybatisflex.codegen)
-    implementation(libs.mybatisflex.core)
+    compileOnly(libs.mybatisflex.core)
     // https://mvnrepository.com/artifact/com.squareup/kotlinpoet-jvm
     implementation(libs.kotlinpoet)
 
